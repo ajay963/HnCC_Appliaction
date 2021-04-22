621464ab91c947a025fbfd2527ddfb3737a8c0fe
Ajay <ajay963@outlook.com>
2021-04-22 21:09:43 +0530

    replacing position widget with translate.transform

--- lib/views/customlogin.dart
+++ lib/views/customlogin.dart
@@ -17,10 +17,8 @@ class _CustomLoginState extends State<CustomLogin> {
     return Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
         body: NuemorphicBackGround(
-            child: SafeArea(
-<<<<<<< HEAD
-          child: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
+            child: SingleChildScrollView(
+          child: SafeArea(
             child: Stack(
               children: [
                 SplashButton(
@@ -33,33 +31,21 @@ class _CustomLoginState extends State<CustomLogin> {
                   offset: Offset(100, 30),
                   child: Row(
                     children: [
-                      getImageAsset(),
-                      getText(
-                          size: 20,
-                          text: 'HnCC',
-                          fontFamily: 'Segoe UI',
-                          fontWeight: FontWeight.w300)
+                      getImageAsset(56, 10, 60),
+                      getText(40, 20, 60, "HnCC", 'Segoe UI', FontWeight.w300)
                     ],
                   ),
                 ),
                 Transform.translate(
                     offset: Offset(30, 240),
                     child: getText(
-                        size: 30,
-                        fontFamily: 'Seoge UI',
-                        text: 'Welcome Corbel',
-                        fontWeight: FontWeight.w600)),
-                Positioned(
-                    top: 305,
-                    left: 35,
-                    child: getText(
-                        size: 23,
-                        text: "Let\'s get started",
-                        fontFamily: 'Segoe UI',
-                        fontWeight: FontWeight.w400)),
-                Positioned(
-                    top: 380,
-                    left: 25,
+                        55, 0, 0, "Welcome", 'Corbel', FontWeight.w400)),
+                Transform.translate(
+                    offset: Offset(35, 305),
+                    child: getText(23, 0, 0, "Let\'s get started", 'Segoe UI',
+                        FontWeight.w400)),
+                Transform.translate(
+                    offset: Offset(25, 490),
                     child: ButtonTapped(
                         height: 56,
                         width: 305,
@@ -77,9 +63,8 @@ class _CustomLoginState extends State<CustomLogin> {
                             textInputAction: TextInputAction.done,
                           ),
                         ))),
-                Positioned(
-                  top: 490,
-                  left: 25,
+                Transform.translate(
+                  offset: Offset(25, 590),
                   child: ButtonTapped(
                       height: 56,
                       width: 305,
@@ -100,103 +85,5 @@ class _CustomLoginState extends State<CustomLogin> {
             ),
           ),
         )));
-  }
-
-  Text getText(
-      {@required String text,
-      @required double size,
-      @required String fontFamily,
-      double height,
-      double letterSpacing,
-      double wordSpacing,
-      FontWeight fontWeight}) {
-    return Text(
-      text,
-      style: TextStyle(
-        fontSize: size,
-        fontFamily: fontFamily,
-        // height: height??1,
-        // letterSpacing: letterSpacing??1,
-        // wordSpacing: wordSpacing??1,
-        fontWeight: fontWeight ?? FontWeight.normal,
-      ),
-    );
-=======
-              child: Stack(
-          children: [
-              Positioned(
-                  top: 181,
-                  left: -253,
-                  child: SplashButton(
-                    height: 755,
-                    width: 755,
-                  )),
-              Positioned(
-                top: 30,
-                right: 100,
-                child: Row(
-                  children: [
-                    getImageAsset(56, 10, 60),
-                    getText(40, 20, 60, "HnCC",'Segoe UI',FontWeight.w300)
-                  ],
-                ),
-              ),
-              Positioned(
-                top: 240,
-                  left: 30,
-                  child: getText(55,0,0,"Welcome",'Corbel',FontWeight.w400)
-              ),
-              Positioned(
-                  top: 305,
-                  left:35,
-                  child: getText(23,0,0,"Let\'s get started",'Segoe UI',FontWeight.w400)
-              ),
-              Positioned(
-                     top: 380,
-                     left: 25,
-                   child:ButtonTapped(
-                     height: 56,
-                     width: 305,
-                     child: SizedBox(
-                     width: 156.0,
-                     child: TextField(
-                         decoration:InputDecoration(
-                           contentPadding: EdgeInsets.only(left: 10,top: 17),
-                           labelText:'Email',
-                           labelStyle: TextStyle(
-                           ),
-                           border: InputBorder.none,
-                         ),
-                         keyboardType: TextInputType.emailAddress,
-                         textInputAction: TextInputAction.done,
-                       ),
-                     )
-                 )
-              ),
-              Positioned(
-                top: 490,
-                left: 25,
-                child:ButtonTapped(
-                    height: 56,
-                    width: 305,
-                    child:  SizedBox(
-                      width: 156.0,
-                      child: TextField(
-                        decoration:InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 10,top: 17),
-                          labelText:'Password',
-                          border: InputBorder.none,
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                        textInputAction: TextInputAction.done,
-                      ),
-                    )
-                ),
-              ),
-
-          ],
-        ),
-            )));
->>>>>>> 6ed494420cb27203f72660218704274d71568b7e
   }
 }
